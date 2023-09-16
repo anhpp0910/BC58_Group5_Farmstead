33595e8c5c7cb485ae44aa1952f14e53626208b5
anhpp0910 <phuonganhphanhhh@gmail.com>
2023-09-16 18:10:08 +0700

    Add dark theme

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
-// SUN MOON SWITCHER
-var changeIcon = function (icon) {
-  icon.classList.toggle("gg-moon");
+// THEME SWITCHER
+document.getElementById("themeSwitcher").onclick = function () {
+  document.getElementById("myBody").classList.toggle("dark");
 };
-
-document.getElementById("theme-switcher").onclick = switchTheme = () => {
-  const rootElem = document.documentElement;
-  let dataTheme = rootElem.getAttribute("data-theme").newTheme;
-  newTheme = dataTheme === "light" ? "dark" : "light";
-  rootElem.setAttribute("data-theme", newTheme);
-};
-
-document
-  .querySelector("#theme-switcher")
-  .addEventListener("click", switchTheme());
