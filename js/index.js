1b3fadd0d22b8495c7736bad7cdfb5fc28a3994d
baomy1411 <baomy1411@gmail.com>
2023-09-17 22:42:00 +0700

    nop bai capstone

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("switchButton").onclick = function () {
+    document.getElementById("myBody").classList.toggle("dark");
+};
+
+// function dropDown(){
+//     var ul = document.getElementById("dropdown");
+//     ul.translate()
+// }
