fccfb50341ca57657fd967a004666513014a7fa4
baomy1411 <baomy1411@gmail.com>
2023-09-20 17:43:01 +0700

    Update All

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
 document.getElementById("switchButton").onclick = function () {
     document.getElementById("myBody").classList.toggle("dark");
-};
-
-// function dropDown(){
-//     var ul = document.getElementById("dropdown");
-//     ul.translate()
-// }
+}
